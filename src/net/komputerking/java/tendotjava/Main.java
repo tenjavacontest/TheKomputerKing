9c97412ae2d6c131401fbd0b31a14efa1bdb28cb
Kailan (TheKomputerKing) Blanks <kailan@komputerking.net>
2013-11-29 20:22:37 +0000

    Added plugin backbone and .gitignore

---
+++ src/net/komputerking/java/tendotjava/Main.java
@@ -0,0 +1,13 @@
+
+package net.komputerking.java.tendotjava;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+   
+    @Override
+    public void onEnable(){
+        
+    }
+    
+}
