a9bd85f63f9b7a7a1ae8942881e086f4981435de
Kailan (TheKomputerKing) Blanks <kailan@komputerking.net>
2013-12-07 19:49:24 +0000

    Added NMS firework with Reflection

--- src/net/komputerking/java/tendotjava/EventListeners.java
+++ src/net/komputerking/java/tendotjava/EventListeners.java
@@ -1,10 +1,15 @@
-
 package net.komputerking.java.tendotjava;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
 import org.bukkit.DyeColor;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Horse;
@@ -25,10 +30,21 @@ public class EventListeners implements Listener {
 
     /**
      * Constructor for the EventListener class.
+     *
      * @param pl - Instance of Main class
      */
     public EventListeners(Main pl) {
         this.pl = pl;
+    }
+
+    public FireworkEffect getFW(boolean good) {
+        if (good) {
+            FireworkEffect temp = FireworkEffect.builder().withColor(Color.GREEN).with(Type.BALL).build();
+            return temp;
+        } else {
+            FireworkEffect temp = FireworkEffect.builder().withColor(Color.RED).with(Type.CREEPER).build();
+            return temp;
+        }
     }
 
     @EventHandler
@@ -39,8 +55,8 @@ public class EventListeners implements Listener {
             event.getClickedBlock().setType(Material.AIR);
             Random rand = new Random();
             Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "EntityCrates" + ChatColor.GRAY + "]" + ChatColor.RED + " " + event.getPlayer().getName() + ChatColor.GREEN + " has found a crate.");
-            for (Player p : Bukkit.getOnlinePlayers()){
-                if (p != event.getPlayer()){
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                if (p != event.getPlayer()) {
                     p.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "EntityCrates" + ChatColor.GRAY + "]" + ChatColor.RED + " Better luck next time!");
                 }
             }
@@ -51,16 +67,30 @@ public class EventListeners implements Listener {
                     Item i = (Item) event.getClickedBlock().getLocation().getWorld().dropItemNaturally(event.getClickedBlock().getLocation(), new ItemStack(Material.DIAMOND, 3));
                     i.setVelocity(new Vector(0, 0.25, 0));
                 }
-                if (seedOfEntity == 0){
-                   Horse h = (Horse) event.getClickedBlock().getLocation().getWorld().spawnEntity(event.getClickedBlock().getLocation(), EntityType.HORSE);
-                   h.setAdult();
-                   h.setTamed(true);
-                   h.setDomestication(1);
-                   h.getInventory().setSaddle(new ItemStack(Material.SADDLE));
+                if (seedOfEntity == 0) {
+                    Horse h = (Horse) event.getClickedBlock().getLocation().getWorld().spawnEntity(event.getClickedBlock().getLocation(), EntityType.HORSE);
+                    h.setAdult();
+                    h.setTamed(true);
+                    h.setDomestication(1);
+                    h.getInventory().setSaddle(new ItemStack(Material.SADDLE));
+                }
+                try {
+                    NMS.doFirework(event.getClickedBlock().getLocation(), getFW(true));
+                } catch (IllegalAccessException ex) {
+                    Logger.getLogger(EventListeners.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InvocationTargetException ex) {
+                    Logger.getLogger(EventListeners.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 event.getPlayer().sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "EntityCrates" + ChatColor.GRAY + "]" + ChatColor.GREEN + " You have found a good crate.");
             } else {
                 // Something Not Nice
+                try {
+                    NMS.doFirework(event.getClickedBlock().getLocation(), getFW(false));
+                } catch (IllegalAccessException ex) {
+                    Logger.getLogger(EventListeners.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InvocationTargetException ex) {
+                    Logger.getLogger(EventListeners.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 event.getPlayer().sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "EntityCrates" + ChatColor.GRAY + "]" + ChatColor.RED + " You have found a bad crate.");
                 int seedOfEntity = rand.nextInt(3);
                 if (seedOfEntity == 1) {
@@ -71,19 +101,19 @@ public class EventListeners implements Listener {
                     }
                 }
                 if (seedOfEntity == 0) {
-                        TNTPrimed t = (TNTPrimed) event.getClickedBlock().getLocation().getWorld().spawnEntity(event.getClickedBlock().getLocation(), EntityType.PRIMED_TNT);
-                        t.setVelocity(new Vector(0, 0.25, 0));
-                        t.setFuseTicks(10);
+                    TNTPrimed t = (TNTPrimed) event.getClickedBlock().getLocation().getWorld().spawnEntity(event.getClickedBlock().getLocation(), EntityType.PRIMED_TNT);
+                    t.setVelocity(new Vector(0, 0.25, 0));
+                    t.setFuseTicks(10);
                 }
-                if (seedOfEntity == 2){
+                if (seedOfEntity == 2) {
                     int amount = 0;
-                    while (amount != 80){
-                    Sheep s = (Sheep) event.getClickedBlock().getLocation().getWorld().spawnEntity(event.getClickedBlock().getLocation(), EntityType.SHEEP);
-                    int colorSeed = rand.nextInt(DyeColor.values().length);
-                    s.setColor(DyeColor.values()[colorSeed]);
-                    Vector velocity = new Vector(rand.nextInt(2), 0.3, rand.nextInt(2));
-                    s.setVelocity(velocity);
-                    amount++;
+                    while (amount != 80) {
+                        Sheep s = (Sheep) event.getClickedBlock().getLocation().getWorld().spawnEntity(event.getClickedBlock().getLocation(), EntityType.SHEEP);
+                        int colorSeed = rand.nextInt(DyeColor.values().length);
+                        s.setColor(DyeColor.values()[colorSeed]);
+                        Vector velocity = new Vector(rand.nextInt(2), 0.3, rand.nextInt(2));
+                        s.setVelocity(velocity);
+                        amount++;
                     }
                 }
             }
--- src/net/komputerking/java/tendotjava/Main.java
+++ src/net/komputerking/java/tendotjava/Main.java
@@ -1,4 +1,3 @@
-
 package net.komputerking.java.tendotjava;
 
 import java.util.Random;
@@ -12,53 +11,53 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin{
-    
+public class Main extends JavaPlugin {
+
     public boolean found = true;
     public Location lastChest = null;
-   
+
     @Override
-    public void onEnable(){
+    public void onEnable() {
         System.out.println("======================================");
         System.out.println("Ten.java Competition Entry - EntityCrates");
         System.out.println("By TheKomputerKing");
         System.out.println("Theme - Entitites");
         System.out.println("======================================");
         Bukkit.getPluginManager().registerEvents(new EventListeners(this), this);
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
-            public void run(){
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            public void run() {
                 dropCrate();
             }
         }, 0L, 10000L);
     }
-    
+
     @Override
-    public void onDisable(){
+    public void onDisable() {
     }
-    
+
     /**
      * Drops a crate at a random location in a loaded chunk.
      */
-    public void dropCrate(){
-        if (found){
-        Random rand = new Random();
-        found = false;
-        final World w = Bukkit.getWorld("world");
-        int chunkSeed = rand.nextInt(w.getLoadedChunks().length);
-        Chunk c = w.getLoadedChunks()[chunkSeed];
-        final Location loc = new Location(w, c.getX(), 255, c.getZ());
-        Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "EntityCrates" + ChatColor.GRAY + "]" + ChatColor.WHITE + " A crate is dropping at the highest point at " + (int) Math.ceil(loc.getX()) + ", " + (int) Math.ceil(loc.getZ()));
-        Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "EntityCrates" + ChatColor.GRAY + "]" + ChatColor.WHITE + " Get there to pick it up.");
-        Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){
-            public void run(){
-              FallingBlock fb = Bukkit.getWorld("world").spawnFallingBlock(loc, Material.CHEST, (byte) 0x0);
-              lastChest = fb.getLocation().getWorld().getHighestBlockAt(fb.getLocation()).getLocation();
-              lastChest.getBlock().setType(Material.AIR);
-              if (lastChest.getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.AIR)){
-                  lastChest.getBlock().getRelative(BlockFace.DOWN).setType(Material.GRASS);
-              }
-            }
-        },120L);
+    public void dropCrate() {
+        if (found) {
+            Random rand = new Random();
+            found = false;
+            final World w = Bukkit.getWorld("world");
+            int chunkSeed = rand.nextInt(w.getLoadedChunks().length);
+            Chunk c = w.getLoadedChunks()[chunkSeed];
+            final Location loc = new Location(w, c.getX(), 255, c.getZ());
+            Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "EntityCrates" + ChatColor.GRAY + "]" + ChatColor.WHITE + " A crate is dropping at the highest point at " + (int) Math.ceil(loc.getX()) + ", " + (int) Math.ceil(loc.getZ()));
+            Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "EntityCrates" + ChatColor.GRAY + "]" + ChatColor.WHITE + " Get there to pick it up.");
+            Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+                public void run() {
+                    FallingBlock fb = Bukkit.getWorld("world").spawnFallingBlock(loc, Material.CHEST, (byte) 0x0);
+                    lastChest = fb.getLocation().getWorld().getHighestBlockAt(fb.getLocation()).getLocation();
+                    lastChest.getBlock().setType(Material.AIR);
+                    if (lastChest.getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.AIR)) {
+                        lastChest.getBlock().getRelative(BlockFace.DOWN).setType(Material.GRASS);
+                    }
+                }
+            }, 120L);
         }
     }
 
