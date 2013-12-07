2de6f09d6b27c592fd38340804aa76ee66f33cfa
Kailan (TheKomputerKing) Blanks <kailan@komputerking.net>
2013-12-07 23:11:47 +0000

    Substituted Splash potion for Potion Effect

--- src/net/komputerking/java/tendotjava/crates/CratePotion.java
+++ src/net/komputerking/java/tendotjava/crates/CratePotion.java
@@ -1,11 +1,12 @@
 package net.komputerking.java.tendotjava.crates;
 
-import java.util.Random;
 import net.komputerking.java.tendotjava.api.Crate;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.potion.PotionType;
 
 public class CratePotion extends Crate {
@@ -17,12 +18,8 @@ public class CratePotion extends Crate {
 
     @Override
     public void onActivate(Location l, Player p) {
-        Location toSpawn = new Location(l.getWorld(), l.getX(), l.getY() + 3, l.getZ());
-        Potion pot = (Potion) l.getWorld().spawnEntity(toSpawn, EntityType.SPLASH_POTION);
-        pot.setSplash(true);
-        pot.setType(PotionType.INSTANT_DAMAGE);
         p.damage(2D);
-        pot.setLevel(2);
+        p.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 15, 1));
     }
 
 }
