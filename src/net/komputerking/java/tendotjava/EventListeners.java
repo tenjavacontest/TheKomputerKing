dbe26dd6c3cc118a853e444d0ec715c57122f88f
Kailan (TheKomputerKing) Blanks <kailan@komputerking.net>
2013-12-07 17:55:36 +0000

    Added bad entity

--- src/net/komputerking/java/tendotjava/EventListeners.java
+++ src/net/komputerking/java/tendotjava/EventListeners.java
@@ -5,36 +5,48 @@ import java.util.Random;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-public class EventListeners implements Listener{
-    
+public class EventListeners implements Listener {
+
     public Main pl;
-    
-    public EventListeners(Main pl){
+
+    public EventListeners(Main pl) {
         this.pl = pl;
     }
-    
+
     @EventHandler
-    public void onClick(PlayerInteractEvent event){
-        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getLocation().equals(pl.lastChest) && !pl.found){
+    public void onClick(PlayerInteractEvent event) {
+        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getLocation().equals(pl.lastChest) && !pl.found) {
             pl.found = true;
             event.setCancelled(true);
             event.getClickedBlock().setType(Material.AIR);
             Random rand = new Random();
             Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "EntityCrates" + ChatColor.GRAY + "]" + ChatColor.RED + " " + event.getPlayer().getName() + ChatColor.GREEN + " has found a crate.");
             Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "EntityCrates" + ChatColor.GRAY + "]" + ChatColor.RED + " Better luck next time!");
-            if (rand.nextBoolean()){
-             // Something Nice
+            if (rand.nextBoolean()) {
+                // Something Nice
+                // int seedOfEntity = rand.nextInt(2);
+                int seedOfEntity = 1;
                 event.getPlayer().sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "EntityCrates" + ChatColor.GRAY + "]" + ChatColor.GREEN + " You have found a good crate.");
             } else {
-             // Something Not Nice
+                // Something Not Nice
                 event.getPlayer().sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "EntityCrates" + ChatColor.GRAY + "]" + ChatColor.RED + " You have found a bad crate.");
+                // int seedOfEntity = rand.nextInt(2);
+                int seedOfEntity = 1;
+                if (seedOfEntity == 1) {
+                    int amount = 0;
+                    while (amount != 40){
+                    event.getClickedBlock().getLocation().getWorld().spawnEntity(event.getClickedBlock().getLocation(), EntityType.BAT);
+                    amount++;
+                    }
+                }
             }
         }
     }
-    
+
 }
