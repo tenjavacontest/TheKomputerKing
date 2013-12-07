362187cf2aaf3afb843729ded52da3e451510cdf
Kailan (TheKomputerKing) Blanks <kailan@komputerking.net>
2013-12-07 16:43:16 +0000

    Added Mob Despawn on Damage

--- src/net/komputerking/java/tendotjava/EventListeners.java
+++ src/net/komputerking/java/tendotjava/EventListeners.java
@@ -1,11 +1,12 @@
 
 package net.komputerking.java.tendotjava;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class EventListeners implements Listener{
@@ -29,11 +30,25 @@ public class EventListeners implements Listener{
                 }
             }
             lastIterated.setPassenger(event.getRightClicked());
-            Bukkit.getScheduler().scheduleSyncDelayedTask(pl, new Runnable(){
-                public void run(){
-                    event.getRightClicked().remove();
+        }
+    }
+    
+    @EventHandler
+    public void onDamage(EntityDamageEvent event){
+        if (event.getEntity() instanceof Player){
+            Player p = (Player) event.getEntity();
+            if (Main.isPlayerInGame(p)){
+            Entity lastIterated = p;
+            boolean stillInProg = true;
+            while(stillInProg){
+                if (lastIterated.getPassenger() != null && lastIterated != p){
+                    lastIterated.remove();
+                    lastIterated = lastIterated.getPassenger();
+                } else {
+                    stillInProg = false;
                 }
-            }, 1500L);
+            }
+            }
         }
     }
     
