ac1242e4e95545105fb7701c81fa712d5ea398f5
Kailan (TheKomputerKing) Blanks <kailan@komputerking.net>
2013-12-07 20:11:23 +0000

    Hopefully fixed Reflection

--- src/net/komputerking/java/tendotjava/NMS.java
+++ src/net/komputerking/java/tendotjava/NMS.java
@@ -1,48 +1,21 @@
 package net.komputerking.java.tendotjava;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.lang.reflect.Field;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.entity.Firework;
 import org.bukkit.inventory.meta.FireworkMeta;
 
 public class NMS {
 
-    private static Method worldHandle = null;
-    private static Method broadcastEntityEffect = null;
-    private static Method fireworkHandle = null;
-
-    public static void doFirework(Location par1, FireworkEffect par2) throws IllegalAccessException, InvocationTargetException {
-        World world = par1.getWorld();
-        Firework fw = (Firework) world.spawn(par1, Firework.class);
-        if (worldHandle == null) {
-            worldHandle = getMethod(world.getClass(), "getHandle");
-            fireworkHandle = getMethod(fw.getClass(), "getHandle");
-        }
-        Object nms_world = worldHandle.invoke(world, (Object[]) null);
-        Object nms_fw = fireworkHandle.invoke(fw, (Object[]) null);
-        if (broadcastEntityEffect == null) {
-            broadcastEntityEffect = getMethod(nms_world.getClass(), "broadcastEntityEffect");
-        }
-        FireworkMeta data = (FireworkMeta) fw.getFireworkMeta();
-        data.clearEffects();
-        data.setPower(1);
-        data.addEffect(par2);
-        fw.setFireworkMeta(data);
-
-        broadcastEntityEffect.invoke(nms_world, new Object[]{nms_fw, (byte) 17});
-        fw.remove();
-    }
-
-    private static Method getMethod(Class<?> c, String m) {
-        for (Method method : c.getMethods()) {
-            if (method.getName().equals(m)) {
-                return method;
-            }
-        }
-        return null;
+    public static void doFirework(Location par1, FireworkEffect par2) throws NoSuchFieldException {
+        Firework fw = (Firework) par1.getWorld().spawn(par1, Firework.class);
+        FireworkMeta fwm = fw.getFireworkMeta();
+        fwm.clearEffects();
+        fwm.addEffect(par2);
+        Field f = fwm.getClass().getDeclaredField("power");
+        f.setAccessible(true);
+        fw.setFireworkMeta(fwm);
     }
 
 }
