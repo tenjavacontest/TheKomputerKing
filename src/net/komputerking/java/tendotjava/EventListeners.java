6c39f3afa824ac050fe21094d0212232d2072a81
Kailan (TheKomputerKing) Blanks <kailan@komputerking.net>
2013-12-07 16:32:45 +0000

    Made it so mobs can stack more than one

--- src/net/komputerking/java/tendotjava/EventListeners.java
+++ src/net/komputerking/java/tendotjava/EventListeners.java
@@ -2,6 +2,7 @@
 package net.komputerking.java.tendotjava;
 
 import org.bukkit.entity.Creature;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
@@ -11,7 +12,16 @@ public class EventListeners implements Listener{
     @EventHandler
     public void onEntityClick(PlayerInteractEntityEvent event){
         if (Main.isPlayerInGame(event.getPlayer()) && event.getRightClicked() instanceof Creature){
-            event.getPlayer().setPassenger(event.getRightClicked());
+            Entity lastIterated = event.getPlayer();
+            boolean stillInProg = true;
+            while(stillInProg){
+                if (lastIterated.getPassenger() != null){
+                    lastIterated = lastIterated.getPassenger();
+                } else {
+                    stillInProg = false;
+                }
+            }
+            lastIterated.setPassenger(event.getRightClicked());
         }
     }
     
