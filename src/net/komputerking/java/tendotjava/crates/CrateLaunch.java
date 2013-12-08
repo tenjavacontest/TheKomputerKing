46114b3591bf6a1196855ee80aa007ec02094e78
Kailan (TheKomputerKing) Blanks <kailan@komputerking.net>
2013-12-08 00:03:21 +0000

    Added Launch Crate

---
+++ src/net/komputerking/java/tendotjava/crates/CrateLaunch.java
@@ -0,0 +1,20 @@
+package net.komputerking.java.tendotjava.crates;
+
+import net.komputerking.java.tendotjava.api.Crate;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+public class CrateLaunch extends Crate {
+    
+    public CrateLaunch() {
+        this.setGood(false);
+        EntityCrates.registerCrate(this);
+    }
+    
+    @Override
+    public void onActivate(Location par1, Player par2) {
+        par2.setVelocity(par2.getVelocity().add(new Vector(0.0, 0.75, 0.0)));
+    }
+    
+}
